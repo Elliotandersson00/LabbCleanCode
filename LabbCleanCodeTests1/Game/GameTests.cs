0142b23343db05d7cdb5930e8521af246bf92127
ElliotAndersson00 <Elliotandersson00@hotmail.com>
2022-11-29 14:26:58 +0100

    Implementation of unit testing

---
+++ LabbCleanCodeTests1/Game/GameTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LabbCleanCode.Game;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabbCleanCode.Game.Tests
+{
+    [TestClass()]
+    public class GameTests
+    {
+        [TestMethod()]
+        public void GenerateCorrectNumbersTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
