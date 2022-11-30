a0e5e73b2f59e1b9a297a171480b81919ad6897c
Emil Törnqvist <emil.tornqvist@iths.se>
2022-11-30 13:37:26 +0100

    added interface folder and moved interface etc

--- LabbCleanCode/Interfaces/IGame.cs
+++ LabbCleanCode/Interfaces/IGame.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace LabbCleanCode.Game
+namespace LabbCleanCode.Interfaces
 {
     internal interface IGame
     {
--- LabbCleanCode/UserData/UserData.cs
+++ LabbCleanCode/UserData/UserData.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LabbCleanCode.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace LabbCleanCode.UserData
 {
-    internal class UserData
+    public class UserData : IUserData
     {
             public string Name { get; private set; }
             public int NGames { get; private set; }
